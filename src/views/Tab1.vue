b0a87885265b4a542b6f9939d234b8f3e881330d
darabosg <sconty@gmail.com>
2021-11-01 22:54:40 +0100

    pwa config2

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,61 +1,85 @@
 <template>
-  <ion-page>
-    <ion-content :fullscreen="true">
-    <ion-header v-if="deferredPrompt">
-      <ion-toolbar>
-        <ion-title>XXX</ion-title>
-        
-        Get our free app. It won't take up space on your phone and also works offline!
-        
-          <ion-button @click="dismiss">Dismiss</ion-button>
-          <ion-button @click="install">Install</ion-button>
-      
-      </ion-toolbar>
-    </ion-header>
-    <ion-header v-else>
-      <ion-title>YYY</ion-title>
-    </ion-header>
-     <ion-list>
-        <ion-item>
-          <ion-label>Blueberry</ion-label>
-          <ion-toggle slot="start" name="blueberry" checked></ion-toggle>
-        </ion-item>
-     </ion-list>
-      <ExploreContainer name="333" />
-    </ion-content>
-  </ion-page>
+    <ion-page>
+        <ion-content :fullscreen="true">
+            <ion-header>
+                <ion-toolbar
+                    ><ion-title>YYY</ion-title>
+                    <ion-button v-if="deferredPrompt" @click="dismiss"
+                        >Dismiss</ion-button
+                    >
+                    <ion-button v-if="deferredPrompt" @click="install"
+                        >Install</ion-button
+                    ></ion-toolbar
+                >
+            </ion-header>
+            <ion-list>
+                <ion-item>
+                    <ion-label>Blueberry</ion-label>
+                    <ion-toggle
+                        slot="start"
+                        name="blueberry"
+                        checked
+                    ></ion-toggle>
+                </ion-item>
+            </ion-list>
+            <ExploreContainer name="333" />
+        </ion-content>
+    </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonItem, IonLabel, IonToggle } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonToggle,
+} from '@ionic/vue'
+import ExploreContainer from '@/components/ExploreContainer.vue'
 
-export default  {
-  name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonList, IonItem, IonLabel, IonToggle },
-  
-  data() {
-    return {
-      deferredPrompt: null
-    };
-  },
-  created() {
-    window.addEventListener("beforeinstallprompt", e => {
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      this.deferredPrompt = e;
-    });
-window.addEventListener("appinstalled", () => {
-      this.deferredPrompt = null;
-    });
-  },
-  methods: {
-    async dismiss() {
-      this.deferredPrompt = null;
+export default {
+    name: 'Tab1',
+    components: {
+        ExploreContainer,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonPage,
+        IonButton,
+        IonList,
+        IonItem,
+        IonLabel,
+        IonToggle,
     },
-    async install() {
-      this.deferredPrompt.prompt();
-    }
-  }
+
+    data() {
+        return {
+            deferredPrompt: null,
+        }
+    },
+    created() {
+        window.addEventListener('beforeinstallprompt', e => {
+            e.preventDefault()
+            // Stash the event so it can be triggered later.
+            this.deferredPrompt = e
+        })
+        window.addEventListener('appinstalled', () => {
+            this.deferredPrompt = null
+        })
+    },
+    methods: {
+        async dismiss() {
+            this.deferredPrompt = null
+        },
+        async install() {
+            this.deferredPrompt.prompt()
+        },
+    },
 }
 </script>
