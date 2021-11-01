6fc822c45101b311123bf8875e5eb6146f4e8ee2
darabosg <sconty@gmail.com>
2021-11-01 20:53:36 +0100

    pwa install test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <ion-app>
+    <ion-header
+        v-if="deferredPrompt"
+      >
+        Get our free app. It won't take up space on your phone and also works offline!
+        
+        <template v-slot:actions>
+          <v-btn text @click="dismiss">Dismiss</v-btn>
+          <v-btn text @click="install">Install</v-btn>
+        </template>
+      </ion-header>
     <ion-router-outlet />
   </ion-app>
 </template>
@@ -13,6 +23,29 @@ export default defineComponent({
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  data() {
+    return {
+      deferredPrompt: null
+    };
+  },
+  created() {
+    window.addEventListener("beforeinstallprompt", e => {
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
+window.addEventListener("appinstalled", () => {
+      this.deferredPrompt = null;
+    });
+  },
+  methods: {
+    async dismiss() {
+      this.deferredPrompt = null;
+    },
+    async install() {
+      this.deferredPrompt.prompt();
+    }
   }
 });
 </script>
