d620e49c4e5b31ec41661fbbe6810003dc028ec6
darabosg <sconty@gmail.com>
2021-11-07 13:06:28 +0100

    change to test update2

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,7 +2,7 @@
     <ion-page>
         <ion-content :fullscreen="true">
             <ion-header>
-                <ion-toolbar><ion-title>XXX</ion-title> </ion-toolbar>
+                <ion-toolbar><ion-title>111</ion-title> </ion-toolbar>
             </ion-header>
             <ion-list>
                 <ion-item>
@@ -14,7 +14,7 @@
                     ></ion-toggle>
                 </ion-item>
             </ion-list>
-            <ExploreContainer name="XXX" />
+            <ExploreContainer name="111" />
         </ion-content>
     </ion-page>
 </template>
